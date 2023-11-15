78217f8b7c3e833dc37b014b2d6750903f84607e
Arrivvaa <vv.var.vfx@gmail.com>
2023-11-15 18:20:29 +0300

    tests refactoring

--- app/tests/test_bag.py
+++ app/tests/test_bag.py
@@ -3,10 +3,10 @@ from service.bag import Bag
 
 class TestBag(unittest.TestCase):
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         
-        self.bag = Bag()
-        self.assertIsInstance(self.bag, Bag, 'Bag instance not create')
+        cls.bag = Bag()
 
 
     def test_get_barrel(self):
